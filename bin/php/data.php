fa81b2f66e18741d1dfe892eb25ebcdc362d6e03
Shaqattack118 <shaq_cameron@hotmail.com>
2015-04-22 15:45:55 -0500

    Testing complete, reset data back to defaults

--- bin/php/data.php
+++ bin/php/data.php
@@ -18,8 +18,8 @@ array("HiScores", "#"),
 //Staff ranks who can access the cpanel
 $staff_ranks = array(4,7,6,8);
 //Staff ids for who can view bans, mutes and other logs
-$canViewLogs = array(4,6);
+$canViewLogs = array(4,7,6);
 //Staff ids for who can ban users, mute etc.
-$canPerfomActions  = array(4,6);
+$canPerfomActions  = array(4,7,6);
 
 ?>
