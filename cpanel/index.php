68c6a2cdb8fc09f3631859d5e51c975b8cd00803
Shaqattack118 <shaq_cameron@hotmail.com>
2015-04-25 18:14:17 -0500

    fix

--- cpanel/index.php
+++ cpanel/index.php
@@ -34,7 +34,7 @@ if(isset($_POST['submitbutton'])) {
 		case "Search Ban": 
 			$query = "SELECT * FROM `banned` WHERE `username` = ? LIMIT ?, ?";
 			$pre = $conn->prepare($query);
-			$pre->execute(array($_POST['sban'], ($page-1)*$resultsPerPage, $resultsPerPage));
+			$pre->execute(array($_POST['sban'], (($page-1)*$resultsPerPage), ($resultsPerPage)));
 			$results = $pre->fetchAll(PDO::FETCH_ASSOC);
 			if(count($results) <= 0) {
 				$data = 'The username you entered cannot be found!';
@@ -47,9 +47,9 @@ if(isset($_POST['submitbutton'])) {
 			}
 			break;
 		case "Search IP(Bans)": 
-			$query = "SELECT * FROM `ipbans` WHERE `ip` = ? LIMIT ?, ?";
+			$query = "SELECT * FROM `ipbans` LIMIT ?, ? WHERE `ip` = ?";
 			$pre = $conn->prepare($query);
-			$pre->execute(array($_POST['siban'], ($page-1)*$resultsPerPage, $resultsPerPage));
+			$pre->execute(array(($_POST['siban']), (($page-1)*$resultsPerPage), ($resultsPerPage)));
 			$results = $pre->fetchAll(PDO::FETCH_ASSOC);
 			if(count($results) <= 0) {
 				$data = 'The ip address you entered cannot be found!';
