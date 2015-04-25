3f1f4881b850826160391fee8c1008517c993933
Shaqattack118 <shaq_cameron@hotmail.com>
2015-04-25 18:16:50 -0500

    test

--- cpanel/index.php
+++ cpanel/index.php
@@ -47,9 +47,9 @@ if(isset($_POST['submitbutton'])) {
 			}
 			break;
 		case "Search IP(Bans)": 
-			$query = "SELECT * FROM `ipbans` WHERE `ip` = ? LIMIT ?, ?";
+			$query = "SELECT * FROM `ipbans` WHERE `ip` = ? LIMIT {($page-1)*$resultsPerPage}, {$resultsPerPage}";
 			$pre = $conn->prepare($query);
-			$pre->execute(array(($_POST['siban']), (($page-1)*$resultsPerPage), ($resultsPerPage)));
+			$pre->execute(array($_POST['siban']));
 			$results = $pre->fetchAll(PDO::FETCH_ASSOC);
 			if(count($results) <= 0) {
 				$data = 'The ip address you entered cannot be found!';
