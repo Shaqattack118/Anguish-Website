9504562804fea87ef015fc5fccc6673ed3a067f2
Shaqattack118 <shaq_cameron@hotmail.com>
2015-04-25 18:59:14 -0500

    update

--- cpanel/index.php
+++ cpanel/index.php
@@ -24,17 +24,25 @@ if(!isLoggedIn) {
 if(!in_array($userInfo['member_group_id'], $staff_ranks)) {
 	header("Location: ../index.php");
 }
-if(isset($_POST['submitbutton'])) {
+if(isset($_POST['submitbutton']) || isset($_GET['action'])) {
+	
+	if(isset($_POST['submitbutton'])) {
+		$fdata = $_POST;
+		$fdata['action'] = $_POST['submitbutton'];
+	} else {
+		$fdata = unserialize($_GET['data']);
+	}
+	
 	if(!isset($_GET['page'])) {
 		$page = 1;
 	} else {
 		$page = $_GET['page'];
 	}
-	switch($_POST['submitbutton']) {
+	switch($fdata['submitbutton']) {
 		case "Search Ban": 
 			$query = "SELECT * FROM `banned` WHERE `username` = ? LIMIT ?, ?";
 			$pre = $conn->prepare($query);
-			$pre->execute(array($_POST['sban'], ($page-1)*$resultsPerPage, $resultsPerPage));
+			$pre->execute(array($fdata['sban'], ($page-1)*$resultsPerPage, $resultsPerPage));
 			$results = $pre->fetchAll(PDO::FETCH_ASSOC);
 			if(count($results) <= 0) {
 				$data = 'The username you entered cannot be found!';
@@ -49,7 +57,7 @@ if(isset($_POST['submitbutton'])) {
 		case "Search IP(Bans)": 
 			$query = "SELECT * FROM `ipbans` WHERE `ip` = ? LIMIT ?, ?";
 			$pre = $conn->prepare($query);
-			$pre->execute(array($_POST['siban'] ($page-1)*$resultsPerPage, $resultsPerPage));
+			$pre->execute(array($fdata['siban'] ($page-1)*$resultsPerPage, $resultsPerPage));
 			$results = $pre->fetchAll(PDO::FETCH_ASSOC);
 			if(count($results) <= 0) {
 				$data = 'The ip address you entered cannot be found!';
