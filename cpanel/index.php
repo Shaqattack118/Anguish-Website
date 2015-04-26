50655f3c2e484fbb06cbcc3d1e7096d86e4f24dd
Shaqattack118 <shaq_cameron@hotmail.com>
2015-04-26 00:27:06 -0500

    finish of pagination for 1 set

--- cpanel/index.php
+++ cpanel/index.php
@@ -68,8 +68,7 @@ if(isset($_POST['submitbutton']) || isset($_GET['data'])) {
 			$pre2 = $conn->prepare("SELECT COUNT(*) FROM `ipbans`");
 			$pre2->execute();
 			$max=$pre2->fetchAll(PDO::FETCH_ASSOC);
-			print_r($max);
-			$max= 2;
+			$max= ceil($max['COUNT(*)']/$resultsPerPage);
 			$results = $pre->fetchAll(PDO::FETCH_ASSOC);
 			$serializedData = urlencode(serialize($fdata));
 			if(count($results) <= 0) {
