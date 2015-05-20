50fb44b5ac52849fa18307fccae66edd55bb18eb
Patrick McDonald <patrick.mcdonald@pixstory.me>
2015-05-20 14:40:13 -0400

    some handling code

--- VoteCallBackApp.js
+++ VoteCallBackApp.js
@@ -4,12 +4,59 @@ var io = require('socket.io')(http);
 
 /** MYSQL */
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
+var connection = initializeConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'rJCa!#7@mgq82hNS',
   database : 'forums'
 });
+
+
+
+function initializeConnection(config) {
+    function addDisconnectHandler(connection) {
+        connection.on("error", function (error) {
+            if (error instanceof Error) {
+                if (error.code === "PROTOCOL_CONNECTION_LOST") {
+                    console.error(error.stack);
+                    console.log("Lost connection. Reconnecting...");
+
+                    initializeConnection(connection.config);
+                } else if (error.fatal) {
+                    throw error;
+                }
+            }
+        });
+    }
+
+    var connection = mysql.createConnection(config);
+
+    // Add handlers.
+    addDisconnectHandler(connection);
+
+    connection.connect();
+    return connection;
+}
+
+
+function handleDisconnect(connection) {
+  connection.on('error', function(err) {
+    if (!err.fatal) {
+      return;
+    }
+
+    if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+      throw err;
+    }
+
+    console.log('Re-connecting lost connection: ' + err.stack);
+
+    connection = mysql.createConnection(connection.config);
+    handleDisconnect(connection);
+    connection.connect();
+  });
+}
+
 
 var activeClients = {};
 
@@ -32,7 +79,7 @@ var activeClients = {};
 
     });
     
-    connection.end();
+    handleDisconnect(connection);
 
  }
 
