b847eaf890f82352b26c67708e2ba22b5f458326
Patrick McDonald <patrick.mcdonald@pixstory.me>
2015-05-20 14:49:53 -0400

    vote callback more

--- VoteCallBackApp.js
+++ VoteCallBackApp.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var requestify = require('requestify');
 /** MYSQL */
 var mysql      = require('mysql');
 var connection = initializeConnection({
@@ -12,61 +12,33 @@ var connection = initializeConnection({
 });
 
 
-
-function initializeConnection(config) {
-    function addDisconnectHandler(connection) {
-        connection.on("error", function (error) {
-            if (error instanceof Error) {
-                if (error.code === "PROTOCOL_CONNECTION_LOST") {
-                    console.error(error.stack);
-                    console.log("Lost connection. Reconnecting...");
-
-                    initializeConnection(connection.config);
-                } else if (error.fatal) {
-                    throw error;
-                }
-            }
-        });
-    }
-
-    var connection = mysql.createConnection(config);
-
-    // Add handlers.
-    addDisconnectHandler(connection);
-
-    connection.connect();
-    return connection;
-}
-
-
-function handleDisconnect(connection) {
-  connection.on('error', function(err) {
-    if (!err.fatal) {
-      return;
-    }
-
-    if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
-      throw err;
-    }
-
-    console.log('Re-connecting lost connection: ' + err.stack);
-
-    connection = mysql.createConnection(connection.config);
-    handleDisconnect(connection);
-    connection.connect();
-  });
-}
-
-
+/** Current clients **/
 var activeClients = {};
 
+/**
+ *  Default route for Callback
+ */
  app.get('/', function(req, res){
     var sessionId = req.query.usr;
     getMemeberId(res, sessionId);
  });
 
-
-
+/**
+ *  Get a valid vote pin from our php API
+ */
+ function createVotePin(){
+ 
+  requestify.post('http://www.anguishps.com/website/api.php', {
+      action: 'createVPin'
+  })
+  .then(function(response) {
+      console.log(response.getBody());
+  });
+ }
+ 
+/**
+ * Get Member Id 
+ */
  function getMemeberId(res, sessionId){
    
     var query = connection.query('SELECT m.member_id FROM `sessions` s, `members` m where s.id = ? and s.member_id = m.member_id', [sessionId], function(err, results) {
@@ -74,6 +46,7 @@ var activeClients = {};
         
               
       res.send('<h1>'+JSON.stringify(results)+'</h1>');
+      createVotePin();
 
     });
     
@@ -116,3 +89,55 @@ io.on('connection', function(socket){
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+
+
+
+/**
+ * Handle Connection for MYSQL node
+ */
+function initializeConnection(config) {
+    function addDisconnectHandler(connection) {
+        connection.on("error", function (error) {
+            if (error instanceof Error) {
+                if (error.code === "PROTOCOL_CONNECTION_LOST") {
+                    console.error(error.stack);
+                    console.log("Lost connection. Reconnecting...");
+
+                    initializeConnection(connection.config);
+                } else if (error.fatal) {
+                    throw error;
+                }
+            }
+        });
+    }
+
+    var connection = mysql.createConnection(config);
+
+    // Add handlers.
+    addDisconnectHandler(connection);
+
+    connection.connect();
+    return connection;
+}
+
+/**
+ * Handle disconnect for MYSQL node
+ */
+function handleDisconnect(connection) {
+  connection.on('error', function(err) {
+    if (!err.fatal) {
+      return;
+    }
+
+    if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+      throw err;
+    }
+
+    console.log('Re-connecting lost connection: ' + err.stack);
+
+    connection = mysql.createConnection(connection.config);
+    handleDisconnect(connection);
+    connection.connect();
+  });
+}
